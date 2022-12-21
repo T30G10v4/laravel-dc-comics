5a83b30e7788c58c2164076f4a5b9e8a8d23ab4f
T30G10v4 <matteo.giovagnotti@gmail.com>
2022-12-21 16:52:21 +0100

    seeder completed

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comic;
+use config\comics;
 
 class ComicsTableSeeder extends Seeder
 {
@@ -15,5 +17,22 @@ class ComicsTableSeeder extends Seeder
     public function run()
     {
         //
+
+        $comics = config('comics');
+        
+        foreach ($comics as $comic) {
+
+            $newComic = new Comic();
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
+            $newComic->save();
+
+        }
+
     }
 }
